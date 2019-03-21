6a2c194378deae9acce41cf75d70f5f5477d267a
leadstar116 <anton.egoro116@yandex.ru>
2019-03-21 18:17:46 +1000

    before adding categories

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-include_once("core/functions.php");
-include_once("includes/header.php");
-include_once("includes/footer.php");
+    include_once("core/functions.php");
+    include_once("includes/header.php");
+?>
+
+<body>
+    <!-- title -->
+    <div class="title text-center">
+        <h3>Categories</h3>
+    </div>    
+    <!-- title end -->
+    <!-- main category section     -->
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+    <!-- main category section end -->
+</body>
+
+<?php
+    include_once("includes/footer.php");
+?>
